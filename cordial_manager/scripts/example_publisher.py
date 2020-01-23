fd660b75dd449a4446437eb27eddcd93db633da3
Audrow <audrow.nash@gmail.com>
2020-01-23 18:56:54 +0000

    Add an example for cordial_manager

---
+++ cordial_manager/scripts/example_publisher.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+
+import rospy
+from std_msgs.msg import String
+
+
+rospy.init_node('example_manager_publisher', anonymous=True)
+say_publisher = rospy.Publisher("/cordial/say", String, queue_size=1)
+
+
+if __name__ == '__main__':
+
+    text_to_say = "Can you hear me?  Is my mouth moving?"
+
+    rospy.sleep(5)
+    while not rospy.is_shutdown():
+
+        rospy.loginfo("Publishing message '{}'".format(text_to_say))
+        say_publisher.publish(text_to_say)
+        rospy.sleep(5)
