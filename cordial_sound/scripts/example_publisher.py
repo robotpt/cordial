823102d0391e00d35c05ecbbed5604cd3f7c613a
Audrow <audrow.nash@gmail.com>
2020-01-31 19:07:27 +0000

    Move ROS param call out of sound play function

--- cordial_sound/scripts/example_publisher.py
+++ cordial_sound/scripts/example_publisher.py
@@ -7,7 +7,7 @@ from std_msgs.msg import String
 
 
 rospy.init_node('example_sound_publisher', anonymous=True)
-sound_publisher = rospy.Publisher("cordial/sound/play/from_file/wav", String, queue_size=1)
+sound_publisher = rospy.Publisher("cordial/sound/play/file_path/wav", String, queue_size=1)
 
 
 if __name__ == '__main__':
@@ -16,11 +16,9 @@ if __name__ == '__main__':
     current_directory = os.path.dirname(os.path.realpath(__file__))
     file_path = os.path.join(current_directory, file_name)
 
-    rospy.sleep(5)
+    rospy.sleep(2)
     while not rospy.is_shutdown():
 
         rospy.loginfo("Publishing sound")
         sound_publisher.publish(file_path)
-        rospy.sleep(5)
-
-
+        rospy.sleep(2)
--- cordial_sound/scripts/sound_listener.py
+++ cordial_sound/scripts/sound_listener.py
@@ -7,32 +7,41 @@ import time
 from cordial_sound.msg import Sound
 
 
+FRAMES_PER_BUFFER = rospy.get_param(
+    'cordial/sound/frames_per_buffer', default=512,
+)
+
+
 def play_sound(data):
 
     rospy.loginfo("Sound received")
 
     p = pyaudio.PyAudio()
 
-    frames_per_buffer = rospy.get_param(
-        'cordial/sound/frames_per_buffer', default=512,
-    )
-    # TODO: figure out why occasionally this line fails
-    stream = p.open(
-        format=data.format,
-        channels=data.num_channels,
-        rate=data.framerate,
-        output=True,
-        frames_per_buffer=frames_per_buffer,
-    )
-    stream.write(data.data)
-    time.sleep(1)
-    stream.stop_stream()
+    """
+    Occasionally pyaudio fails to open correctly.  Online resources said that this was likely because This is an attempt to fix it by closing down the node
+    and restarting the node. 
+    """
+    try:
+        stream = p.open(
+            format=data.format,
+            channels=data.num_channels,
+            rate=data.framerate,
+            output=True,
+            frames_per_buffer=FRAMES_PER_BUFFER,
+        )
+        stream.write(data.data)
+        time.sleep(1)
+        stream.stop_stream()
 
-    stream.close()
-    p.terminate()
+        stream.close()
+        p.terminate()
+    except IOError:
+        rospy.signal_shutdown("Audio device appears to be busy")
 
 
 if __name__ == '__main__':
+
 
     rospy.init_node('sound_listener')
     rospy.Subscriber('cordial/sound/play/stream', Sound, play_sound)
