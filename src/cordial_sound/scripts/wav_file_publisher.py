88faa4f94cc096423a215f5bb37059a746a0feac
Anand Gokul <anandgokul18@gmail.com>
2020-12-28 14:31:23 +0530

    Reverting Commit and Rebasing to make changes
    Signed-off-by: Anand Gokul <anandgokul18@gmail.com>

--- src/cordial_sound/scripts/wav_file_publisher.py
+++ src/cordial_sound/scripts/wav_file_publisher.py
@@ -4,6 +4,10 @@ import rospy
 import pyaudio
 import numpy as np
 import wave
+from pydub import AudioSegment
+import math
+import os
+import tempfile
 
 from cordial_msgs.msg import Sound
 from std_msgs.msg import String
@@ -18,21 +22,33 @@ class WavFilePublisher:
             'cordial/sound/wav/header_length',
             24
         )
-        rospy.Subscriber(rospy.get_param('cordial_sound/play_wav_topic'), String, self.play_wav_file)
-        self._sound_publisher = rospy.Publisher(rospy.get_param('cordial_sound/play_stream_topic'), Sound, queue_size=1)
+        rospy.Subscriber(rospy.get_param(
+            'cordial_sound/play_wav_topic'), String, self.play_wav_file)
+        self._sound_publisher = rospy.Publisher(rospy.get_param(
+            'cordial_sound/play_stream_topic'), Sound, queue_size=10)
         self._pyaudio = pyaudio.PyAudio()
 
-    def play_wav_file(self, data):
+    def _split_audio_once(self, from_sec, to_sec, audio):
+        t1 = from_sec * 1000  # seconds to milliseconds
+        t2 = to_sec * 1000  # seconds to milliseconds
+        split_audio = audio[t1:t2]
+        return split_audio
 
-        file_path = data.data
+    def split_audio_into_chunks(self, filename, seconds_per_split):
+        audio = AudioSegment.from_wav(filename)
+        total_seconds = math.ceil(audio.duration_seconds)
+        audio_chunks = []
+        for i in range(0, total_seconds, seconds_per_split):
+            split_audio = self._split_audio_once(i, i+seconds_per_split, audio)
+            audio_chunks.append(split_audio)
+            if i == total_seconds - seconds_per_split:
+                rospy.loginfo(
+                    'The audio file splited successfully into chunks')
+        return audio_chunks
 
-        try:
-            wf = wave.open(file_path, 'rb')
-        except IOError:
-            rospy.logerr("Not a valid wav file: '{}'".format(file_path))
-            return
-
-        audio_format = self._pyaudio.get_format_from_width(wf.getsampwidth())
+    def publish_audio_chunk(self, wf, file_path, current_chunk_id):
+        audio_format = self._pyaudio.get_format_from_width(
+            wf.getsampwidth())
         framerate = wf.getframerate()
         num_channels = wf.getnchannels()
 
@@ -45,8 +61,34 @@ class WavFilePublisher:
         sound_msg.framerate = framerate
         sound_msg.data = data
 
-        rospy.loginfo("Publishing sound from '{}'".format(file_path))
+        rospy.loginfo(
+            f"Publishing sound chunk {str(current_chunk_id)}/{str(len(self.audio_chunks))} from '{format(file_path)}'")
         self._sound_publisher.publish(sound_msg)
+
+    def play_wav_file(self, data, seconds_per_split=1):
+
+        file_path = data.data
+
+        try:
+            wf_original = wave.open(file_path, 'rb')
+        except IOError:
+            rospy.logerr("Not a valid wav file: '{}'".format(file_path))
+            return
+
+        # Splitting wav to multiple parts of 1 sec each
+        audio_chunks = self.split_audio_into_chunks(
+            file_path, seconds_per_split=seconds_per_split)
+
+        # Creating a temporary file for the audio
+        temp_audio_file = tempfile.NamedTemporaryFile(suffix='.wav')
+
+        # Publishing each chunk of 1 sec separtely
+        for i, value in enumerate(audio_chunks):
+            value.export(temp_audio_file.name, format="wav")
+
+            wf = wave.open(temp_audio_file.name, 'rb')
+
+            self.publish_audio_chunk(wf, file_path, i)
 
 
 if __name__ == '__main__':
